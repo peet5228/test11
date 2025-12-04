4c5429db20aad94d4b063ea01ae82bdde8538458
peet5228 <ppeet5228@gmail.com>
2025-12-04 11:04:58 +0700

    api

--- backend/routes/Staff/member.js
+++ backend/routes/Staff/member.js
@@ -27,4 +27,67 @@ router.get('/eva',verifyToken,requireRole('ฝ่ายบุคลากร'),a
     }
 })
 
+// API สำหรับ GET ข้อมูล
+router.get('/commit',verifyToken,requireRole('ฝ่ายบุคลากร'),async (req,res) => {
+    try{
+        const [rows] = await db.query(`select * from tb_member where role='กรรมการประเมิน' order by id_member descc`)
+        res.json(rows)
+    }catch(err){
+        console.error('Error Get',err)
+        res.status(500).json({message:'Error Get'})
+    }
+})
+
+// API สำหรับ GET ข้อมูล
+router.get('/all',verifyToken,requireRole('ฝ่ายบุคลากร'),async (req,res) => {
+    try{
+        const [rows] = await db.query(`select * from tb_member order by id_member descc`)
+        res.json(rows)
+    }catch(err){
+        console.error('Error Get',err)
+        res.status(500).json({message:'Error Get'})
+    }
+})
+
+// API สำหรับ GET ข้อมูล where params
+router.get('/:id_member',verifyToken,requireRole('ฝ่ายบุคลากร'),async (req,res) => {
+    try{
+        const {id_member} = req.params
+        const [rows] = await db.query(`select * from tb_member where id_member='${id_member}' order by id_member descc`)
+        if(rows.length === 0) return res.status(403).json({message:'ไม่พบข้อมูลจากไอดีนี้'})
+        res.json(rows)
+    }catch(err){
+        console.error('Error Get',err)
+        res.status(500).json({message:'Error Get'})
+    }
+})
+
+
+// API สำหรับ UPDATE ข้อมูล
+router.put('/:id_member',verifyToken,requireRole('ฝ่ายบุคลากร'),async (req,res) => {
+    try{
+        const {id_member} = req.body
+        const {first_name,last_name,email,username,password,role} = req.body
+        const [rows] = await db.query(`update tb_member set first_name=?,last_name=?,email=?,username=?,password=?,role=? where id_member='${id_member}'`,[first_name,last_name,email,username,password,role])
+        res.json({rows,message:'Update Success'})
+    }catch(err){
+        console.error('Error Update',err)
+        res.status(500).json({message:'Error Update'})
+    }
+})
+
+// API สำหรับ DELTE ข้อมูล
+router.delete('/:id_member',verifyToken,requireRole('ฝ่ายบุคลากร'),async (req,res) => {
+    try{
+        const {id_member} = req.body
+        const [rows] = await db.query(`delete from tb_member where id_member='${id_member}'`)
+        if(rows.affectedRows === 0) return res.status(403).json({message:'ไม่พบข้อมูลจากไอดีนี้'})
+        res.json({rows,message:'delete Success'})
+    }catch(err){
+        console.error('Error Delete',err)
+        res.status(500).json({message:'Error Delete'})
+    }
+})
+
+
 module.exports = router
---
+++ backend/routes/Staff/topic.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const db = require('../../db')
+const router = express.Router()
+const {verifyToken,requireRole} = require('../../middleware/authMiddleware')
+
+// ============ DEMO ============
+// API สำหรับ GET ข้อมูล
+// router.get('/',verifyToken,requireRole('ฝ่ายบุคลากร'),async (req,res) => {
+//     try{
+//         const [rows] = await db.query(``)
+//         res.json(rows)
+//     }catch(err){
+//         console.error('Error Get',err)
+//         res.status(500).json({message:'Error Get'})
+//     }
+// })
+// ============ DEMO ============
+
+// API สำหรับ GET ข้อมูล
+router.get('/',verifyToken,requireRole('ฝ่ายบุคลากร'),async (req,res) => {
+    try{
+        const [rows] = await db.query(`select * from tb_topic order by id_topic desc`)
+        res.json(rows)
+    }catch(err){
+        console.error('Error Get',err)
+        res.status(500).json({message:'Error Get'})
+    }
+})
+
+// API สำหรับ GET ข้อมูล where params
+router.get('/:id_topic',verifyToken,requireRole('ฝ่ายบุคลากร'),async (req,res) => {
+    try{
+        const {id_topic} = req.params
+        const [rows] = await db.query(`select * from tb_topic where id_topic='${id_topic}' order by id_topic desc`)
+        if(rows.length === 0) return res.status(403).json({message:'ไม่พบข้อมูลจากไอดีนี้'})
+        res.json(rows)
+    }catch(err){
+        console.error('Error Get',err)
+        res.status(500).json({message:'Error Get'})
+    }
+})
+
+// API สำหรับ INSERT ข้อมูล
+router.post('/',verifyToken,requireRole('ฝ่ายบุคลากร'),async (req,res) => {
+    try{
+        const {name_topic} = req.body
+        const [rows] = await db.query(`insert into tb_topic (name_topic) values (?)`,[name_topic])
+        res.json({rows,message:'Insert Success'})
+    }catch(err){
+        console.error('Error Insert',err)
+        res.status(500).json({message:'Error Insert'})
+    }
+})
+
+// API สำหรับ Update ข้อมูล
+router.update('/:id_topic',verifyToken,requireRole('ฝ่ายบุคลากร'),async (req,res) => {
+    try{
+        const {id_topic} = req.params
+        const {name_topic} = req.body
+        const [rows] = await db.query(`update tb_topic set name_topic=? where id_topic='${id_topic}'`,[name_topic])
+        res.json({rows,message:'Update Success'})
+    }catch(err){
+        console.error('Error Update',err)
+        res.status(500).json({message:'Error Update'})
+    }
+})
+
+// API สำหรับ Delete ข้อมูล
+router.delete('/:id_topic',verifyToken,requireRole('ฝ่ายบุคลากร'),async (req,res) => {
+    try{
+        const {id_topic} = req.params
+        const [rows] = await db.query(`delete from tb_topic where id_topic='${id_topic}'`)
+        if(rows.affectedRows === 0) return res.status(403).json({message:'ไม่พบข้อมูลจากไอดีนี้'})
+        res.json({rows,message:'Delete Success'})
+    }catch(err){
+        console.error('Error Delete',err)
+        res.status(500).json({message:'Error Delete'})
+    }
+})
+
+
+module.exports = router
